a96d95926f4c3cf09efe7bddbd627e5a5122a2a5
i-Vanbi <62582216+i-Vanbi@users.noreply.github.com>
2023-07-02 19:15:30 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -20,12 +20,18 @@ sed -i 's/192.168.1.1/10.1.1.8/g' package/base-files/files/bin/config_generate
 rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
+rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/aria2
+rm -rf feeds/packages/net/ariang
+
+
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-adbyby-plus
 
 # 添加额外插件
 # git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
